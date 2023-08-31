70350ae9e99a4fa83d4d22f808b4630230ed5e2c
les-rodriguez <lesli.rodriguezz98@gmail.com>
2023-08-30 23:08:10 -0600

    on and off boxes

---
+++ app.js
@@ -0,0 +1,225 @@
+$('#clickMe1').click(function() {
+    let isSwitched = $('#clickMe1').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe1').removeClass('switch');
+    $('#clickMe1').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe1').addClass('switch');
+    $('#clickMe1').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe2').click(function() {
+    let isSwitched = $('#clickMe2').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe2').removeClass('switch');
+    $('#clickMe2').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe2').addClass('switch');
+    $('#clickMe2').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe3').click(function() {
+    let isSwitched = $('#clickMe3').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe3').removeClass('switch');
+    $('#clickMe3').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe3').addClass('switch');
+    $('#clickMe3').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe4').click(function() {
+    let isSwitched = $('#clickMe4').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe4').removeClass('switch');
+    $('#clickMe4').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe4').addClass('switch');
+    $('#clickMe4').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe5').click(function() {
+    let isSwitched = $('#clickMe5').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe5').removeClass('switch');
+    $('#clickMe5').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe5').addClass('switch');
+    $('#clickMe5').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe6').click(function() {
+    let isSwitched = $('#clickMe6').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe6').removeClass('switch');
+    $('#clickMe6').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe6').addClass('switch');
+    $('#clickMe6').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe7').click(function() {
+    let isSwitched = $('#clickMe7').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe7').removeClass('switch');
+    $('#clickMe7').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe7').addClass('switch');
+    $('#clickMe7').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe8').click(function() {
+    let isSwitched = $('#clickMe8').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe8').removeClass('switch');
+    $('#clickMe8').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe8').addClass('switch');
+    $('#clickMe8').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe9').click(function() {
+    let isSwitched = $('#clickMe9').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe9').removeClass('switch');
+    $('#clickMe9').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe9').addClass('switch');
+    $('#clickMe9').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe10').click(function() {
+    let isSwitched = $('#clickMe10').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe10').removeClass('switch');
+    $('#clickMe10').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe10').addClass('switch');
+    $('#clickMe10').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe11').click(function() {
+    let isSwitched = $('#clickMe11').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe11').removeClass('switch');
+    $('#clickMe11').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe11').addClass('switch');
+    $('#clickMe11').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe12').click(function() {
+    let isSwitched = $('#clickMe12').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe12').removeClass('switch');
+    $('#clickMe12').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe12').addClass('switch');
+    $('#clickMe12').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe13').click(function() {
+    let isSwitched = $('#clickMe13').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe13').removeClass('switch');
+    $('#clickMe13').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe13').addClass('switch');
+    $('#clickMe13').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe14').click(function() {
+    let isSwitched = $('#clickMe14').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe14').removeClass('switch');
+    $('#clickMe14').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe14').addClass('switch');
+    $('#clickMe14').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe15').click(function() {
+    let isSwitched = $('#clickMe15').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe15').removeClass('switch');
+    $('#clickMe15').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe15').addClass('switch');
+    $('#clickMe15').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+$('#clickMe16').click(function() {
+    let isSwitched = $('#clickMe16').hasClass('switch');
+if (isSwitched) {
+    $('#clickMe16').removeClass('switch');
+    $('#clickMe16').css({
+      'background-color': 'black'
+    });
+  } else {
+    $('#clickMe16').addClass('switch');
+    $('#clickMe16').css({
+      'background-color': 'grey'
+    });
+}
+}); 
+
